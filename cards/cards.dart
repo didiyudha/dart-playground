e13f934fa7b83be8d220135ccb179dfb8f1ac778
Didi Yudha Perwira <didiyudha@gmail.com>
2018-07-28 17:41:32 +0700

    initialize card in deck constructor

--- cards/cards.dart
+++ cards/cards.dart
@@ -1,8 +1,32 @@
+void main(List<String> args) {
+  var deck = new Deck();
+
+  print("here's the cards");
+  deck.cards.forEach((card) => 
+    print("${card.rank} of ${card.suit}")
+  );
+}
+
 class Deck {
-  List<Card> cards;
+  List<Card> cards = [];
+
+  Deck() {
+    var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five'];
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+
+    for (var suit in suits) {
+      for(var rank in ranks) {
+        var card = new Card(suit, rank);
+        this.cards.add(card);
+      }
+    }
+  }
+  
 }
 
 class Card {
   String suit;
   String rank;
+
+  Card(this.suit, this.rank);
 }
