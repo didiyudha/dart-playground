8cc0d837fdbe8735aef2b5f0eb0d798c3c411174
Didi Yudha Perwira <didiyudha@gmail.com>
2018-07-28 10:58:13 +0700

    dynamic type in dart

--- first-program/myname.dart
+++ first-program/myname.dart
@@ -1,8 +1,19 @@
 void main() {
+
+  // String type
   var name = myName();
+
+  // Dynamic type
+  var work = myWork();
+
   print('My name is $name');
+  print('Im working as $work');
 }
 
 String myName() {
   return 'Didi Yudha';
+}
+
+myWork() {
+  return 'Software Engineer';
 }
