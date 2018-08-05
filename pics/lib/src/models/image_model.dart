6dc9b1a33b64f8142192e60729a6c9ea06cbabe3
Didi Yudha Perwira <didiyudha@gmail.com>
2018-08-05 16:43:54 +0700

    change the form of name constructor

--- pics/lib/src/models/image_model.dart
+++ pics/lib/src/models/image_model.dart
@@ -5,9 +5,14 @@ class ImageModel {
 
   ImageModel(this.id, this.title, this.url);
 
-  ImageModel.fromJson(Map<String, dynamic> parsedJson) {
-    id = parsedJson['id'];
-    title = parsedJson['title'];
-    url = parsedJson['url'];
-  }
+  // ImageModel.fromJson(Map<String, dynamic> parsedJson) {
+  //   id = parsedJson['id'];
+  //   title = parsedJson['title'];
+  //   url = parsedJson['url'];
+  // }
+
+  ImageModel.fromJson(Map<String, dynamic> parsedJson) 
+    : id = parsedJson['id'], 
+      title = parsedJson['title'], 
+      url =parsedJson['url'];
 }
